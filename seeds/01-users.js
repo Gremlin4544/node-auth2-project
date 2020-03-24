cfd476ac962b6cef40130edf6be5ef96d1f462d4
Gremlin4544 <alve@mckethen.com>
2020-03-24 17:03:07 -0400

    updated users table and changed del to truncate

--- seeds/01-users.js
+++ seeds/01-users.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('users').insert([
+        {username: 'Galadriel', password: 'light', department: 'Lothlorien'},
+        {username: 'Gandalf', password: 'glamdring', department: 'Istari'},
+        {username: 'Gollum', password: 'precious', department: 'Caves'}
       ]);
     });
 };
